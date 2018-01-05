a56f792e131ff2d65f3c3c103ef7c9d16234afa2
sabe111 <1512670@hs-mannheim.de>
2018-01-05 18:31:31 +0100

    encrypt decypt

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include "crypto.h"
 
-bool checkInputText(char* input) {
+bool checkInputText(const char* input, const char* checkSet) {
   bool isLegal = true;
-  char legalCharacters[] = MESSAGE_CHARACTERS;
   char ckey;
+
+  if(input ==   '\0'){
+    return false;
+  }
 
 for (int i = 0; input[i] != '\0'; i++){
   ckey = input[i];
+  if(isLegal == false) {
+    return false;
+  }
+  isLegal = false;
+  for (int k = 0; checkSet[i] != '\0'; k++) {
+    if(ckey == checkSet[i]){
+      isLegal = true;
+    }
+  }
+}
+return true;
+}
+
+bool checkKey(KEY key){
+  bool isLegal = true;
+  char legalCharacters[] = KEY_CHARACTERS;
+  char ckey;
+  if(key.chars == '\0') {
+    return false;
+  }
+
+for (int i = 0; key.chars[i] != '\0'; i++){
+  ckey = key.chars[i];
   if(isLegal == false) {
     return false;
   }
@@ -22,20 +50,63 @@ for (int i = 0; input[i] != '\0'; i++){
 return true;
 }
 
-int checkKey(KEY key){
+void extendKey (const char* input, char* ouput, int increase) {
+  int i, k;
+  int length = strlen(input);
 
+  for (i = 0, k = 0; i < increase; i++, k++) {
+    if(k == length){
+      k = 0;
+    }
+  }
+}
+
+void xor (KEY key, const char* input, char* output) {
+  if (key.type == 1) {
+    char* extendedKey = (char*) malloc(sizeof(char) * (strlen(input) + 1));
+    extendKey(key.chars, extendedKey, strlen(input));
+
+    for (int i = 0; i < strlen(input); i++) {
+      output[i] = output[i] - 'A' + 1;
+      extendedKey[i] = extendedKey[i] - 'A' + 1;
+
+      output[i] = output[i] ^ extendedKey[i];
+      output[i] = output[i] + 'A' -1;
+    }
+    free(extendedKey);
+
+  } else {
+    *output = *input;
+  }
 }
 
 
 int encrypt(KEY key, const char* input, char* output){
-  if (key.type == 1) {
-
-
-
-  } else {
-    *output = *input;
-    return 0;
+  if(strlen(key.chars) == 0){
+    return E_KEY_TOO_SHORT;
   }
+  else if(!checkKey(key)) {
+    return E_KEY_ILLEGAL_CHAR;
+  }
+  else if (!checkInputText(input, MESSAGE_CHARACTERS)) {
+    return E_MESSAGE_ILLEGAL_CHAR;
+  } else {
+    xor(key, input, output);
+  }
+  return 0;
+}
 
-
+int decrypt(KEY key, const char* cypherText, char* output){
+  if(strlen(key.chars) == 0){
+    return E_KEY_TOO_SHORT;
+  }
+  else if(!checkKey(key)) {
+    return E_KEY_ILLEGAL_CHAR;
+  }
+  else if (!checkInputText(cypherText, CYPHER_CHARACTERS)) {
+    return E_MESSAGE_ILLEGAL_CHAR;
+  } else {
+    xor(key, cypherText, output);
+  }
+  return 0;
 }
--- crypter/main.c
+++ crypter/main.c
@@ -1,5 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "crypto.h"
 
- int main() {
+ int main(int argc, char* argv[]) {
+   if(argc == 1) {
+     printf("Keine Parameter \n");
+   }
 
-   return 1;
+   KEY key = {1, argv[1]};
+   char input[100];
+   int returnValue;
+   char* result;
+
+   if (argc == 2) {
+     fgets(input, 100, stdin);
+     result = (char*) malloc(sizeof(char) * strlen(input));
+     if(strstr(argv[0], "encrypt")) {
+       returnValue = encrypt(key, input, result);
+     } else if (strstr(argv[0], "decrypt")) {
+       returnValue = decrypt(key, input, result);
+     }
+     printf("%s \n", result);
+     free(result);
+   }
+
+   switch(returnValue) {
+     case E_KEY_TOO_SHORT:
+      fprintf(stderr, "Key zu kurz"); break;
+     case E_KEY_ILLEGAL_CHAR:
+      fprintf(stderr, "Ungültige Zeichen in Key");break;
+     case E_MESSAGE_ILLEGAL_CHAR:
+      fprintf(stderr, "Ungültige Zeichen in Nachricht"); break;
+     case E_CYPHER_ILLEGAL_CHAR:
+      fprintf(stderr, "Ungültige Zeichen in verschlüsseltem Text"); break;
+     default: return returnValue;
+   }
  }
