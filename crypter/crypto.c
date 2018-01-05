2c55a06f0fbb3de8ef46df4d6737aafef6a4b866
sabe111 <1512670@hs-mannheim.de>
2018-01-05 15:37:38 +0100

    check input text

---
+++ crypter/crypto.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "crypto.h"
+
+bool checkInputText(char* input) {
+  int containsIllegalChar = 0;
+  int count = 0;
+  bool isLegal = true;
+  char legalCharacters[] = MESSAGE_CHARACTERS;
+  char ckey;
+
+for (int i = 0; input[i] != '\0'; i++){
+  ckey = input[i];
+  if(isLegal == false) {
+    return false;
+  }
+  isLegal = false;
+  for (int k = 0; legalCharacters[i] != '\0'; k++) {
+    if(ckey == legalCharacters[i]){
+      isLegal = true;
+    }
+  }
+
+}
+
+
+}
+
+int checkKey(KEY key){
+
+}
+
+
+int encrypt(KEY key, const char* input, char* output){
+  if (key.type == 1) {
+
+
+
+  } else {
+    *output = *input;
+    return 0;
+  }
+
+
+}
